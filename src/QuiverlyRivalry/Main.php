abe00a7655b222b9f7c60002d23498fb27a6bfbc
TBNRItzDoge <33814351+TBNRItzDoge@users.noreply.github.com>
2017-12-21 18:39:02 -0500

    Update Main.php

--- src/QuiverlyRivalry/Main.php
+++ src/QuiverlyRivalry/Main.php
@@ -14,12 +14,12 @@ class Main extends PluginBase implements Listener
 {
     public function onLoad()
     {
-        $this->getLogger()->info("Plugin Loading, castleraidepe.tk 19132, join xD!");
+        $this->getLogger()->info("Plugin Loading, nitronetwork.ddns.net 19132, join xD!");
     }
 
     public function onEnable()
     {
-        $this->getLogger()->info("plugin enabled!");
+        $this->getLogger()->info("plugin enabled!, Use /book in-game to use.");
     }
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
@@ -30,11 +30,11 @@ class Main extends PluginBase implements Listener
                     if ($sender instanceof Player) {
                         $book = Item::get(Item::WRITTEN_BOOK, 0, 1);
                         $book->setTitle(C::GREEN . C::UNDERLINE . "Information Booklet");
-                        $book->setPageText(0, C::GREEN . C::UNDERLINE . "Whats a Kingdom?" . C::BLACK . "\n - A kingdom, is your home, its like a fations. Except bigger! \n - Kingdoms, have many members and a custom world! \n - Each kingdom has a king, this king is who you shall fight for!");
-                        $book->setPageText(1, C::GREEN . C::UNDERLINE . "How can my Kingdom win?" . C::BLACK . "\n - You can earn power in the weekly wars, and from PvPing enemy kingdoms! \n - You can earn power in our KOTH at warzone.");
-                        $book->setPageText(2, C::GREEN . c::UNDERLINE . "How do I store my loot, and get loot?" . C::BLACK . "\n - Try doing /pv 1, for a vault! \n - Go to your kingdoms world, and make a base, skybase, or lair! \n - Make sure you raid other kingdoms bases!");
-                        $book->setPageText(3, C::GREEN . c::UNDERLINE . "Helpful Commands" . C::BLACK . "\n- /k \n - /warpme \n - /pv \n - /shop \n - /potions \n - /ulog \n - /cp");
-                        $book->setAuthor("CastleRaid Network");
+                        $book->setPageText(0, C::GREEN . C::UNDERLINE . "What the Nitro Network?" . C::BLACK . "\n - The Nitro Network is a semi-custom Factions Server. We are factions, but Nitro. \n That means that we have a fast host, good plugins, and lots of cool features!");
+                        $book->setPageText(1, C::GREEN . C::UNDERLINE . "How can my Faction get F-Top 1??" . C::BLACK . "\n - You can earn power in faction wars, pvp, koths, and raiding enemy factions! \n - You can also earn power by recruiting members to your faction.");
+                        $book->setPageText(2, C::GREEN . c::UNDERLINE . "How do I store my loot, and get loot?" . C::BLACK . "\n - Try doing /uv 1, for a vault! \n - Go to your Factions world, and make a base, skybase, or lair! \n - Make sure you raid other factions bases for success!");
+                        $book->setPageText(3, C::GREEN . c::UNDERLINE . "Helpful Commands" . C::BLACK . "\n- /f topfactions \n - /warpui \n - /uv \n - /shop \n - /mask \n - /ulog \n - /cp");
+                        $book->setAuthor("the Nitro Network Team");
                         $sender->getInventory()->addItem($book);
                     }
                 }
